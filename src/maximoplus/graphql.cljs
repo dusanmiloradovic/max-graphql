56585192116165c15f1d32c4a3c1b104cfa2b673
Dusan Miloradovic <dusan.m@maximoplus.com>
2018-08-15 21:30:21 +0400

    nodejs impementation of network maximoplus protocol

--- src/maximoplus/graphql.cljs
+++ src/maximoplus/graphql.cljs
@@ -3,17 +3,12 @@
             [maximoplus.core :as c]
             [maximoplus.net :as n]
             [maximoplus.utils :as u]
-            ["xhr2-cookies" :refer [XMLHttpRequest]]
-            ["cookiejar" :as cookies :refer [CookieJar]]
-            ["eventsource" :as EventSource])
+            [maximoplus.net.node :refer [Node]]
+)
   (:require-macros [maximoplus.macros :as mm :refer [def-comp googbase kk! kk-nocb! kk-branch-nocb! p-deferred p-deferred-on custom-this kc!]]
                    [cljs.core.async.macros :refer [go]]))
 
-(aset XMLHttpRequest "cookiejar" (CookieJar.))
-
-(aset js/global "XMLHttpRequest" XMLHttpRequest)
-
-(aset js/global "EventSource" EventSource)
+(n/set-net-type (Node.))
 
 (n/set-server-root "http://localhost:8080")
 
---
+++ src/maximoplus/net/node.cljs
@@ -0,0 +1,64 @@
+(ns maximoplus.net.node
+  (:require
+   [maximoplus.net :as n :refer [INet]]
+   [maximoplus.utils :as u :refer [transit-read]]
+   ["request" :as request]
+   ["eventsource" :as EventSource])
+  )
+
+(def -req (.defaults request #js{:jar true}))
+;;we can put jar true, because oll the variables are global for one session, so we have to spawn this from the main nodejs process (and then there is no need to have multiple jars)
+
+(def tabsess (atom "123"))
+
+(def event-source (atom nil))
+
+(defn send-data
+  [option callback error-callback]
+  (-req option 
+        (fn [err resp body]
+          (if err
+            (error-callback [err 6 (.statusCode resp)]) ;;For the compatibility reasons with browser, I will use just 6 (error) and 0 no error
+            (callback [(transit-read body) 0 (.statusCode resp)])))))
+
+(deftype Node []
+  INet
+  (-send-get
+    [this url callback error-callback]
+    (-send-get this url nil callback error-callback))
+  (-send-get;;ingore data, them later just remove this method, I think it is obsolette
+    [this url data callback error-callback]
+    (send-data url callback error-callback))
+  (-send-post
+    [this url data callback error-callback]
+    (-send-post this url data callback error-callback nil))
+  (-send-post
+    [this url data callback error-callback progress-callback];;ignore progress for the time, i don't think it is relevant at this point
+    (send-data #js{:url url
+                   :body data
+                   :method "POST"} callback error-callback))
+  (-start-server-push-receiving
+    [this callback error-callback]
+    (let [sse-url (n/sse)
+          ev-s (EventSource. sse-url #js {:withCredentials true})]
+      (reset! event-source ev-s)
+      (.addEventListener ev-s "message"
+                         (fn [message]
+                           (let [_data (aget message "data")
+                                 data (if (= "" _data) "" (u/transit-read _data))]
+                             (u/debug "SSE" data)
+                             (callback data))))
+      (.addEventListener ev-s "error"
+                         (fn [error] (error-callback error)))))
+  (-stop-server-push-receiving
+    [this]
+    (when @event-source
+      (.close @event-source)
+      (reset! event-source nil)))
+  (-get-tabsess;;tabsess handling will be done by the implemntation (browser or node)
+    [this]
+    @tabsess)
+  (-set-tabsess!
+    [this _tabsess]
+    (reset! tabsess _tabsess))
+  )
