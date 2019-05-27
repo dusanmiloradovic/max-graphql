b350d467452b558f768399facdeab8a8c6393491
Dusan Miloradovic <dusan@gasco.ae>
2019-05-27 15:46:16 +0400

    winpath

--- winpath.bat
+++ winpath.bat
@@ -1,4 +1,5 @@
-set PATH=c:\dusan\jdk1.8\bin;%PATH%;c:\dusan\node-v7.9.0-win-x64\;c:\dusan\lein2.6\
+rem set PATH=c:\dusan\jdk1.8\bin;%PATH%;c:\dusan\node-v7.9.0-win-x64\;c:\dusan\lein2.6\
+set PATH=c:\dusan\jdk1.8\bin;%PATH%;c:\dusan\node-v10.15.3-win-x64\;c:\dusan\lein2.6\
 set http_proxy=http://localhost:3128
 set https_proxy=http://localhost:3128
 rem lein run -m shadow.cljs.devtools.cli watch server
